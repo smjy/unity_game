ffc63a7a18fc73e9f437ab116edf58dedf23a192
VicRanger <wzzcjbb@163.com>
2018-04-28 12:18:40 +0800

    地图边框卡牢初步

---
+++ Assets/Toolkits/Minimap/Scripts/MinimapIconController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class MinimapIconController : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	RectTransform rect;
+	void Start () {
+	}
+	void Awake(){
+		rect = GetComponent<RectTransform>();
+	}
+	// Update is called once per frame
+	void Update () {
+        rect = GetComponent<RectTransform>();
+		Vector2 pos = Camera.main.WorldToViewportPoint(target.transform.position);
+		transform.localPosition = new Vector2(Mathf.Clamp((pos.x-0.5f)*180,-90,90),Mathf.Clamp((pos.y - 0.5f) * 180, -90, 90));
+	}
+}
---
+++ Assets/Toolkits/Minimap/Scripts/MinimapManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapManager : MonoBehaviour {
+	public static MinimapManager _instance;
+	public GameObject iconDisplay;
+    public GameObject iconPrefab;
+	static public MinimapManager instance{
+		get{
+			if(_instance == null){
+				_instance = FindObjectOfType<MinimapManager>();
+			}
+			return _instance;
+		}
+	}
+	public void RegisterObj(Transform obj){
+		GameObject icon = Instantiate(iconPrefab,iconDisplay.transform);
+		icon.transform.parent = iconDisplay.transform;
+		icon.GetComponent<MinimapIconController>().target = obj;
+	}
+	public void RemoveObj(Transform obj){
+
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
