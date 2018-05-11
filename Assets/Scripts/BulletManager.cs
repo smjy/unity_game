0d26c58bc3e082cce88424ebd2a4cf8961244507
Vitech Liu <liuzirui1122@163.com>
2018-05-11 23:09:47 +0800

    准心系统完善，现在可以自定义新准心和流畅切换准心
    1.准心切换
    2.删除原准心动画，现在准心动画完全由脚本控制状态(CursorStatus)
    3. 炮塔仍然有bug

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -20,12 +20,7 @@ public class BulletManager : MonoBehaviour {
 
     
 	void Start () {
-		Debug.Log(carnoon.rotation);
-		Debug.Log(carnoon.localRotation);
-        
-        Debug.Log(carnoon.eulerAngles);
 
-        Debug.Log(carnoon.localEulerAngles);
 	}
 	
 	// Update is called once per frame
@@ -53,8 +48,8 @@ public class BulletManager : MonoBehaviour {
         pos.z = 100f;
         Vector3 start_pos = MainPlayer_Single.me.transform.position;
         Vector3 direction = (pos - start_pos).normalized;
-        direction += Random.Range(0f, CursorFollow.main.accuracyRadius) * Random.onUnitSphere;
-        CursorFollow.main.addSpeed(kick);
+        direction += Random.Range(0f, CursorController.main.accuracyRadius) * Random.onUnitSphere;
+        CursorController.main.addSpeed(kick);
         create(start_pos + direction * 10f, direction);
     }
     void create(Vector3 position, Vector3 direction) {
