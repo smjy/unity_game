4412bdeda11d2238e19e747f7f75456a7c738ac3
jimengying <38742946+jimengying@users.noreply.github.com>
2018-05-17 10:24:41 +0800

    按钮状态更改
    Button控件状态监测，处于enter状态改变材质，exit状态改变状态

---
+++ Assets/Scripts/ButtonStatus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestUiEvent : MonoBehaviour
+{
+    public Material hoverMt;
+    public Material normalMt;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        UIEventListener btnListener = btn.gameObject.AddComponent<UIEventListener>();
+
+        btnListener.OnMouseEnter += delegate (GameObject gb) {
+            Debug.Log(gb.name + " OnMouseEnter");
+            gb.GetComponent<Image>().material = hoverMt;
+        };
+
+        btnListener.OnMouseExit += delegate (GameObject gb) {
+            Debug.Log(gb.name + " OnMOuseExit");
+            gb.GetComponent<Image>().material = normalMt;
+        };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
