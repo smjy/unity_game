a451b5d8e5ab28209ad2557adde67e3e85e029a5
jimengying <38742946+jimengying@users.noreply.github.com>
2018-05-17 20:10:05 +0800

    音量滑条的禁用
    点击按钮实现音量滑条禁用（静音）效果

---
+++ Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClicks : MonoBehaviour {
+    public GameObject setWindow;
+
+	public void on_setButton_click()
+    {
+        Debug.Log("设置按钮点击");
+        setWindow.SetActive(true);
+
+    }
+    public void on_quitButton_click()
+    {
+        Debug.Log("关闭窗口");
+        setWindow.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/VoiceEnable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceEnable : MonoBehaviour {
+
+    public Slider voiceSlider;
+
+	public void on_Button_Checked()
+    {       
+       voiceSlider.interactable = !voiceSlider.interactable;
+    }
+}
