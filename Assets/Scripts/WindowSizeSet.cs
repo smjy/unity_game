f84b34001275dca44b9269a0027e1f395a8918e9
jimengying <38742946+jimengying@users.noreply.github.com>
2018-06-10 10:20:43 +0800

    界面尺寸设置

---
+++ Assets/Scripts/WindowSizeSet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+public class WindowSizeSet : MonoBehaviour {
+
+   
+    public Toggle full, window;   
+    string windowSize = "Full";
+	// Use this for initialization
+	void Start () {
+        if(windowSize=="Full")
+        {
+            full.isOn = true;
+        }
+        else
+        {
+            window.isOn = true;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    public void on_fullActive()
+    {
+        PlayerSettings.defaultIsFullScreen = true;
+        windowSize = "Full";
+    }
+    public void on_windowActive()
+    {
+        PlayerSettings.defaultIsFullScreen = false;
+        windowSize = "Window";
+    }
+}
